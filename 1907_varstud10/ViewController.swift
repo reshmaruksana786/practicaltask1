35e42ee480c892ebbc1dd57bcef00165434d6abb
reshma <reshmaruksana786786@gmail.com>
2020-01-11 11:43:56 +0530

    assigned variables

--- 1907_varstud10/ViewController.swift
+++ 1907_varstud10/ViewController.swift
@@ -11,7 +11,99 @@ import UIKit
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
+    
         super.viewDidLoad()
+        var stud1telugumarks:UInt8 = 90
+        var stud1hindimarks:UInt8 = 85
+        var stud1englishmarks:UInt8 = 75
+        var stud1mathsmarks:UInt8 = 95
+        var stud1sciencemarks:UInt8 = 85
+        var stud1socialmarks:UInt8 = 75
+        
+        
+        var stud2telugumarks:UInt8 = 65
+        var stud2hindimarks:UInt8 = 75
+        var stud2englishmarks:UInt8 = 85
+        var stud2mathsmarks:UInt8 = 95
+        var stud2sciencemarks:UInt8 = 75
+        var stud2socialmarks:UInt8 = 55
+        
+        
+        var stud3telugumarks:UInt8 = 55
+        var stud3hindimarks:UInt8 = 65
+        var stud3englishmarks:UInt8 = 75
+        var stud3mathsmarks:UInt8 = 85
+        var stud3sciencemarks:UInt8 = 95
+        var stud3socialmarks:UInt8 = 65
+        
+        
+        var stud4telugumarks:UInt8 = 55
+        var stud4hindimarks:UInt8 = 66
+        var stud4englishmarks:UInt8 = 68
+        var stud4mathsmarks:UInt8 = 99
+        var stud4sciencemarks:UInt8 = 77
+        var stud4socialmarks:UInt8 = 63
+        
+        
+        var stud5telugumarks:UInt8 = 66
+        var stud5hindimarks:UInt8 = 88
+        var stud5englishmarks:UInt8 = 77
+        var stud5mathsmarks:UInt8 = 55
+        var stud5sciencemarks:UInt8 = 99
+        var stud5socialmarks:UInt8 = 55
+        
+        
+        var stud6telugumarks:UInt8 = 59
+        var stud6hindimarks:UInt8 = 77
+        var stud6englishmarks:UInt8 = 55
+        var stud6mathsmarks:UInt8 = 77
+        var stud6sciencemarks:UInt8 = 66
+        var stud6socialmarks:UInt8 = 55
+        
+        
+        var stud7telugumarks:UInt8 = 88
+        var stud7hindimarks:UInt8 = 55
+        var stud7englishmarks:UInt8 = 66
+        var stud7mathsmarks:UInt8 = 54
+        var stud7sciencemarks:UInt8 = 88
+        var stud7socialmarks:UInt8 = 66
+        
+        var stud8telugumarks:UInt8 = 55
+        var stud8hindimarks:UInt8 = 99
+        var stud8englishmarks:UInt8 = 77
+        var stud8mathsmarks:UInt8 = 68
+        var stud8sciencemarks:UInt8 = 98
+        var stud8socialmarks:UInt8 = 88
+        
+        
+        var stud9telugumarks:UInt8 = 98
+        var stud9hindimarks:UInt8 = 68
+        var stud9englishmarks:UInt8 = 95
+        var stud9mathsmarks:UInt8 = 77
+        var stud9sciencemarks:UInt8 = 98
+        var stud9socialmarks:UInt8 = 85
+        
+        var stud10telugumarks:UInt8 = 88
+        var stud10hindimarks:UInt8 = 77
+        var stud10englishmarks:UInt8 = 99
+        var stud10mathsmarks:UInt8 = 77
+        var stud10sciencemarks:UInt8 = 99
+        var stud10socialmarks:UInt8 = 99
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
